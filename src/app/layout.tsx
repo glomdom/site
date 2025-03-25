3af9f411bd5a33d8c72e19cdc7759748a2793338
glomdom <22115@uktc-bg.com>
2025-03-25 22:13:32 +0200

    feat: add ogp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,14 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "glomdom",
   description: "Created by glomdom",
+  openGraph: {
+    type: "website",
+    url: "https://nexisys.net",
+    title: "glomdom's site or something",
+    description: "look at my beautiful creations",
+    siteName: "glomdom's portfolio",
+    images: [{ url: "https://nexisys.net/_next/image?url=%2Fpfp.jpg&w=3840&q=75" }]
+  }
 };
 
 export default function RootLayout({
